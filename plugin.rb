56dd6ac047aa4e5a65cce16ec0bbea3ac88de4f7
Erick Guan <fantasticfears@gmail.com>
2016-04-27 20:11:43 +0200

    set up

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# name: Master Controls
+# About: Internal for controlling master nodes for DiscourseCN
+# author: Erick
+# version: 0.99
+
+PLUGIN_NAME = "master_controls".freeze
+
+enabled_site_setting :master_controls_enabled
+
+after_initialize do
+end
