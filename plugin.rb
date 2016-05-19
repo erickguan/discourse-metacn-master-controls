61ea4c65a7616873f732aac6ecea8143c59a295d
Erick Guan <fantasticfears@gmail.com>
2016-05-19 21:22:34 +0200

    first controls

--- plugin.rb
+++ plugin.rb
@@ -8,4 +8,9 @@ PLUGIN_NAME = "master_controls".freeze
 enabled_site_setting :master_controls_enabled
 
 after_initialize do
+  class ::ApplicationController
+    def mini_profiler_enabled?
+      defined?(Rack::MiniProfiler) && current_user&.single_sign_on_record&.external_id == 'fantasticfears'
+    end
+  end
 end
