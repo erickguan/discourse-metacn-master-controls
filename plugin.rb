6c542ac3298444e7c0311f99f7aaddbb52105364
Erick Guan <fantasticfears@gmail.com>
2018-11-20 11:41:49 +0100

    put site settings here

--- plugin.rb
+++ plugin.rb
@@ -15,6 +15,43 @@ TOPIC_THRESHOLD = 10
 POST_THRESHOLD = 50
 
 after_initialize do
+  SiteSetting.email_editable=false
+  SiteSetting.has_login_hint=false
+  SiteSetting.enable_sso=true
+  SiteSetting.sso_url='https://meta.discoursecn.org/session/sso_provider'
+  SiteSetting.sso_secret=']cU4+7fddFthK%46ULgRJa,xBJ69gtaJ'
+  SiteSetting.sso_overrides_email=true
+  SiteSetting.sso_overrides_username=true
+  SiteSetting.sso_overrides_name=true
+  SiteSetting.sso_not_approved_url='https://meta.discoursecn.org'
+  SiteSetting.allow_user_locale=true
+  SiteSetting.login_required=true
+  SiteSetting.default_email_digest_frequency=0
+  SiteSetting.default_email_private_messages=false
+  SiteSetting.default_email_direct=false
+  SiteSetting.force_https=true
+  SiteSetting.allow_index_in_robots_txt=false
+  SiteSetting.allow_moderators_to_create_categories=true
+  SiteSetting.enable_user_directory=false
+  SiteSetting.hide_user_profiles_from_public=true
+  SiteSetting.bootstrap_mode_min_users=0
+  SiteSetting.wizard_enabled=false
+  SiteSetting.min_post_length = 2
+  SiteSetting.min_first_post_length = 2
+  SiteSetting.min_private_message_post_length = 2
+  SiteSetting.body_min_entropy = 1
+  SiteSetting.min_topic_title_length = 2
+  SiteSetting.max_topic_title_length = 2
+  SiteSetting.title_min_entropy = 2
+  SiteSetting.title_prettify = false
+  SiteSetting.min_title_similiar_length = 2
+  SiteSetting.min_body_similiar_length = 2
+  SiteSetting.unique_posts_mins = 0
+  SiteSetting.rate_limit_new_user_create_topic = 20
+  SiteSetting.rate_limit_new_user_create_post = 7
+  SiteSetting.disable_digest_emails = true
+  SiteSetting.min_trust_to_allow_self_wiki = 0
+
   class ::ApplicationController
     def mini_profiler_enabled?
       defined?(Rack::MiniProfiler) && current_user&.single_sign_on_record&.external_id == 'fantasticfears'
